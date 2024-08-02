310a8872b0ad5743a5028e8f1b6f143f8f1454d7
blinklabs-admin <mike@blinklabs.ai>
2024-08-02 13:01:16 +0800

    Update the contract with new feature of Token Vesting: Add a token vesting feature using OpenZeppelin's VestingWallet contract. Implement a function to create vesting schedules for different beneficiaries, specifying the amount of tokens to be vested and the vesting duration.

--- hardhat/contracts/CustomToken.sol
+++ hardhat/contracts/CustomToken.sol
@@ -3,17 +3,20 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/finance/VestingWallet.sol";
 
 /**
  * @title CustomToken
- * @dev Implementation of a custom ERC20 token with max supply and minting capabilities.
+ * @dev This contract implements an ERC20 token with vesting capabilities.
  */
-contract CustomToken is ERC20, ERC20Burnable, Ownable {
+contract CustomToken is ERC20, ERC20Burnable, Pausable, Ownable {
     uint256 private immutable _maxSupply;
+    mapping(address => VestingWallet) private _vestingWallets;
 
     /**
-     * @dev Constructor that sets the name, symbol, and max supply of the token.
+     * @dev Constructor that sets the token name, symbol, and maximum supply.
      * @param name_ The name of the token.
      * @param symbol_ The symbol of the token.
      * @param maxSupply_ The maximum supply of the token.
@@ -25,17 +28,33 @@ contract CustomToken is ERC20, ERC20Burnable, Ownable {
 
     /**
      * @dev Returns the maximum supply of the token.
-     * @return The maximum supply of the token.
+     * @return The maximum supply.
      */
     function maxSupply() public view returns (uint256) {
         return _maxSupply;
     }
 
     /**
-     * @dev Mints new tokens to the specified address.
-     * @param to The address to receive the minted tokens.
+     * @dev Pauses all token transfers.
+     * @notice Can only be called by the contract owner.
+     */
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     * @notice Can only be called by the contract owner.
+     */
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    /**
+     * @dev Mints new tokens.
+     * @param to The address that will receive the minted tokens.
      * @param amount The amount of tokens to mint.
-     * @notice Only the contract owner can call this function.
+     * @notice Can only be called by the contract owner.
      */
     function mint(address to, uint256 amount) public onlyOwner {
         require(totalSupply() + amount <= _maxSupply, "Minting would exceed max supply");
@@ -43,12 +62,67 @@ contract CustomToken is ERC20, ERC20Burnable, Ownable {
     }
 
     /**
-     * @dev Overrides the _mint function to enforce the max supply limit.
-     * @param account The address receiving the minted tokens.
-     * @param amount The amount of tokens to mint.
+     * @dev Creates a vesting schedule for a beneficiary.
+     * @param beneficiary The address of the beneficiary.
+     * @param amount The amount of tokens to be vested.
+     * @param startTimestamp The start time of the vesting period.
+     * @param durationSeconds The duration of the vesting period in seconds.
+     * @notice Can only be called by the contract owner.
      */
-    function _mint(address account, uint256 amount) internal virtual override {
-        require(totalSupply() + amount <= _maxSupply, "ERC20: mint amount exceeds max supply");
-        super._mint(account, amount);
+    function createVestingSchedule(
+        address beneficiary,
+        uint256 amount,
+        uint64 startTimestamp,
+        uint64 durationSeconds
+    ) public onlyOwner {
+        require(_vestingWallets[beneficiary] == VestingWallet(payable(address(0))), "Vesting schedule already exists for beneficiary");
+        require(amount > 0, "Vesting amount must be greater than zero");
+        require(durationSeconds > 0, "Vesting duration must be greater than zero");
+
+        VestingWallet newVestingWallet = new VestingWallet(
+            beneficiary,
+            startTimestamp,
+            durationSeconds
+        );
+
+        _vestingWallets[beneficiary] = newVestingWallet;
+        _mint(address(newVestingWallet), amount);
+    }
+
+    /**
+     * @dev Releases vested tokens for a beneficiary.
+     * @param beneficiary The address of the beneficiary.
+     */
+    function releaseVestedTokens(address beneficiary) public {
+        VestingWallet vestingWallet = _vestingWallets[beneficiary];
+        require(address(vestingWallet) != address(0), "No vesting schedule found for beneficiary");
+
+        uint256 releasable = vestingWallet.releasable(address(this));
+        if (releasable > 0) {
+            vestingWallet.release(address(this));
+        }
+    }
+
+    /**
+     * @dev Returns the vesting wallet address for a beneficiary.
+     * @param beneficiary The address of the beneficiary.
+     * @return The address of the vesting wallet.
+     */
+    function getVestingWallet(address beneficiary) public view returns (address) {
+        return address(_vestingWallets[beneficiary]);
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens.
+     * @param from The address tokens are transferred from.
+     * @param to The address tokens are transferred to.
+     * @param amount The amount of tokens to be transferred.
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
